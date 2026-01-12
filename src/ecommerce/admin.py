c291f63d5209ae1b9b29e907d6e79bfa11ea3e7b
mitkos <mitko.sodev@gmail.com>
2026-01-13 00:14:20 +0200

    feat(ecommerce): add category and product models

--- src/ecommerce/admin.py
+++ src/ecommerce/admin.py
@@ -1,2 +1,17 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "parent")
+    search_fields = ("name",)
+    list_filter = ("parent",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "sku", "title", "price", "category", "updated_at")
+    search_fields = ("sku", "title")
+    list_filter = ("category",)
--- src/ecommerce/models.py
+++ src/ecommerce/models.py
@@ -21,6 +21,9 @@ class Category(models.Model):
             )
         ]
 
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self) -> str:
         return self.name
 
